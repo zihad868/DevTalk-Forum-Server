541f31b8b6039611e1a9f4540606b106128e8720
zihad868 <zihadh868@gmail.com>
2024-06-04 22:05:57 +0600

    Delete User API

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ async function run() {
        res.send(result);
     })
 
+    app.delete('/users/:id', async(req, res) => {
+       const id = req.params.id;
+       const filter = {_id: new ObjectId(id)}
+       const result = await userCollections.deleteOne(filter);
+       res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
