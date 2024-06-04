82a76687a3e3a612a5f11840a23cf78460f566a7
zihad868 <zihadh868@gmail.com>
2024-06-04 22:47:23 +0600

    JWT Token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
