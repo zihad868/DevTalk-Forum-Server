0b5e8fb83d558ea8a2ece8f64f2b5d067f218640
zihad868 <zihadh868@gmail.com>
2024-06-04 12:31:48 +0600

    Insert user in database

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -24,12 +25,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const userCollections = client.db('DevTalk').collection('users');
+
+
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+
+        // find user is exist
+        const query = {email: user.email}
+        const existingUser = await userCollections.findOne(query);
+        if(existingUser){
+          return res.send({message: 'user already exist', insertedId: null})
+        }
+
+        const result = await userCollections.insertOne(user);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
